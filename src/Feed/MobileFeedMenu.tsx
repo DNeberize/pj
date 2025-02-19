761030cad128b81cd6da24a5cacee65f3e167006
DNeberize <davitneberidzee@gmail.com>
2025-02-19 17:40:48 +0400

    Update MobileFeedMenu.tsx

--- src/Feed/MobileFeedMenu.tsx
+++ src/Feed/MobileFeedMenu.tsx
@@ -22,29 +22,32 @@ const MobileFeedMenu: React.FC<MobileFeedMenuProps> = ({
   return (
     <div className="gap-y-2 lg:hidden w-full flex flex-col">
       <div className="hidden w-full justify-between rounded-[12px] max-lg:flex">
-        {MenuItems.map((e) => (
+        {MenuItems.map((item, index) => (
           <button
-            key={e.id}
-            className={`rounded-[8px] py-[12px] mobile-menu-p-sizedown text-[#23262E] text-[12px] cursor-pointer font-semibold px-[1rem] flex items-center justify-center shrink-0 transition-colors duration-500 ease-in-out ${
-              selectedTab.slice(0, 3) === e.label.slice(0, 3)
-                ? "bg-[#7F3FFC] text-[#23262E]"
-                : "text-[#23262E]/70"
-            }`}
-            onClick={() => handleTabChange(e.label)}
+            key={item.id}
+            className={`rounded-[8px] py-[12px] text-[12px] cursor-pointer font-semibold 
+              px-[1rem] flex items-center justify-center shrink-0 transition-colors duration-500 ease-in-out
+              ${
+                selectedTab === item.label
+                  ? "bg-[#7F3FFC] text-white"
+                  : "text-[#23262E]/70"
+              }`}
+            onClick={() => handleTabChange(item.label)}
           >
-            {e.label}
+            {index === 0 ? item.label.slice(0, 3) : item.label}
           </button>
         ))}
       </div>
+
       <div className="grid grid-cols-[10fr_1fr] gap-4">
         <div className="h-[36px] rounded-[8px] min-w-[130px] bg-[#F7F8FA] flex justify-between p-[12px] items-center">
           <img
             className="rotate-90 size-[10px]"
             src="src/assets/Vector.svg"
-            alt=""
+            alt="Arrow"
           />
           <div className="flex gap-4 items-center">
-            <img src="src/assets/calendar.svg" alt="" />
+            <img src="src/assets/calendar.svg" alt="Calendar" />
             <p className="text-[11px] text-[#23262E] font-semibold">
               {formattedDate}
             </p>
@@ -52,11 +55,11 @@ const MobileFeedMenu: React.FC<MobileFeedMenuProps> = ({
           <img
             className="-rotate-90 size-[11px]"
             src="src/assets/Vector.svg"
-            alt=""
+            alt="Arrow"
           />
         </div>
         <button className="cursor-pointer hover:opacity-80 bg-[#F7F8FA] border-[#23262E]/[10%] flex items-center justify-center rounded-[8px] border-[1px] border-solid w-[36px]">
-          <img src="src/assets/history.svg" alt="" />
+          <img src="src/assets/history.svg" alt="History" />
         </button>
       </div>
     </div>
